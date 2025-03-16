f887d8a22d02b707cf677b95dbe771aa3b4791e7
Олексій <gavrilov.lucky@gmail.com>
2025-03-16 22:18:38 +0200

    js-02

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,11 @@
 function checkForSpam(message) {
-  if (message.indexOf("spam") !== -1 || message.indexOf("sale") !== -1) {
-    return true;  
+  const lowerMessage = message.toLowerCase();
+  if (lowerMessage.includes('spam') || lowerMessage.includes('sale')) {
+    return true;
+  } else {
+    return false;
   }
-  return false; 
 }
-
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
